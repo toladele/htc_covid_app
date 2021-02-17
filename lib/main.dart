77cbc92847b36d17612a4bf60253b861203becb0
tomiwa <toladele99@gmail.com>
2021-02-16 22:21:57 -0500

    fix sizeconfig error

--- lib/main.dart
+++ lib/main.dart
@@ -63,6 +63,7 @@ class _MyHomePageState extends State<MyHomePage> {
     // The Flutter framework has been optimized to make rerunning build methods
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
+    SizeConfig().init(context);
     return Scaffold(
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
