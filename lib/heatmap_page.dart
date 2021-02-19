8e8103adf49fe50deddca128d075a5d0e044d62c
tomiwa <toladele99@gmail.com>
2021-02-19 18:50:02 -0500

    generate heatmap

--- lib/heatmap_page.dart
+++ lib/heatmap_page.dart
@@ -3,7 +3,8 @@ import 'dart:async';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
+// import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:google_maps_flutter_heatmap/google_maps_flutter_heatmap.dart';
 
 class HeatMapPage extends StatefulWidget {
   @override
@@ -13,11 +14,17 @@ class HeatMapPage extends StatefulWidget {
 class _HeatMapPageState extends State<HeatMapPage> {
   Completer<GoogleMapController> _controller = Completer();
   Position position;
-  LatLng _center = new LatLng(43.549384, -79.662611);
+  LatLng _center = LatLng(43.549384, -79.662611);
+  final Set<Heatmap> _heatmaps = {};
+  CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(43.549384, -79.662611),
+    zoom: 14.4746,
+  );
+  LatLng _heatmapLocation = LatLng(43.549384, -79.662611);
 
-  void _onMapCreated(GoogleMapController controller) {
-    _controller.complete(controller);
-  }
+  // void _onMapCreated(GoogleMapController controller) {
+  //   _controller.complete(controller);
+  // }
 
   void initState() {
     super.initState();
@@ -29,18 +36,49 @@ class _HeatMapPageState extends State<HeatMapPage> {
     position = await Geolocator.getCurrentPosition();
     setState(() {
       _center = LatLng(position.latitude, position.longitude);
+      CameraPosition(
+        target: LatLng(position.latitude, position.longitude),
+        zoom: 14.4746,
+      );
     });
   }
 
   @override
   Widget build(BuildContext context) {
+    _addHeatmap();
     return GoogleMap(
-      onMapCreated: _onMapCreated,
-      myLocationEnabled: true,
-      initialCameraPosition: CameraPosition(
-        target: _center,
-        zoom: 13.0,
-      ),
-    );
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },
+        myLocationEnabled: true,
+        heatmaps: _heatmaps,
+        initialCameraPosition: _kGooglePlex);
+  }
+
+  void _addHeatmap() {
+    setState(() {
+      _heatmaps.add(Heatmap(
+          heatmapId: HeatmapId(_heatmapLocation.toString()),
+          points: _createPoints(_heatmapLocation),
+          radius: 50,
+          visible: true,
+          gradient: HeatmapGradient(
+              colors: <Color>[Colors.green, Colors.red],
+              startPoints: <double>[0.2, 0.8])));
+    });
+  }
+
+  //heatmap generation helper functions
+  List<WeightedLatLng> _createPoints(LatLng location) {
+    final List<WeightedLatLng> points = <WeightedLatLng>[];
+    //Can create multiple points here
+    points.add(_createWeightedLatLng(location.latitude, location.longitude, 5));
+    points.add(
+        _createWeightedLatLng(location.latitude - 1, location.longitude, 5));
+    return points;
+  }
+
+  WeightedLatLng _createWeightedLatLng(double lat, double lng, int weight) {
+    return WeightedLatLng(point: LatLng(lat, lng), intensity: weight);
   }
 }
